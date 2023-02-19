af192c736b48c74827f16ea725aa2ffe1e08b992
Ankui <youshihou5@gmail.com>
2023-02-19 17:59:17 +0800

    update code

--- sources/AttributedStringBuilder/Tests/AttributedStringBuilderTests/AttributedStringBuilderTests.swift
+++ sources/AttributedStringBuilder/Tests/AttributedStringBuilderTests/AttributedStringBuilderTests.swift
@@ -1,5 +1,33 @@
 import XCTest
 @testable import AttributedStringBuilder
+import SwiftUI
+
+
+
+@AttributedStringBuilder
+var example: some AttributedStringConvertible {
+    "Hello World!"
+        .bold()
+    Array(repeating:
+    """
+    This is some markdown with **strong** text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus, tortor eu maximus gravida, ante diam fermentum magna, in gravida ex tellus ac purus.
+    
+    Another *paragraph*.
+    """.markdown(stylesheet: CustomStylesheet()) as AttributedStringConvertible, count: 20)
+    NSImage(systemSymbolName: "hand.wave", accessibilityDescription: nil)!
+    Embed(proposal: .init(width: 200, height: nil)) {
+        HStack {
+            Image(systemName: "hand.wave")
+            Text("Hello from SwiftUI")
+            Color.red.frame(width: 100, height: 50)
+        }
+    }
+}
+
+let sampleAttributes = Attributes(family: "Menlo", size: 16)
+
+
+
 
 final class AttributedStringBuilderTests: XCTestCase {
     func testExample() throws {
@@ -7,5 +35,14 @@ final class AttributedStringBuilderTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
 //        XCTAssertEqual(AttributedStringBuilder().text, "Hello, World!")
+    }
+    
+    @MainActor
+    func testPDF() {
+        let data = example
+            .joined(separator: "\n")
+            .run(environment: .init(attributes: sampleAttributes))
+            .pdf()
+        try? data.write(to: .desktopDirectory.appending(component: "out.pdf"))
     }
 }
